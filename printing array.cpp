5d527762b4c001aaf1170966c0cde94e37c2a0f0
Dev <devkdarji@gmail.com>
2025-08-09 16:05:15 +0530

    Create printing array.cpp

---
+++ printing array.cpp
@@ -0,0 +1,14 @@
+//Dev Darji
+//24070123033
+//ENTC-A2
+
+#include <iostream>
+using namespace std;
+int main() {
+    int array[]={1,3,4,5,6,7,8};
+    for(int i=0;i<7;i++){
+        cout<<array[i]<<" ";        
+    }
+
+    return 0;
+}
