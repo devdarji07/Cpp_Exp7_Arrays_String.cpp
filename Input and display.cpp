6ee0b291d542994eb0bedc14c04b984da3186556
Dev <devkdarji@gmail.com>
2025-08-09 16:02:49 +0530

    Create Input and display.cpp

---
+++ Input and display.cpp
@@ -0,0 +1,15 @@
+//Dev Darji
+//24070123033
+//ENTC-A2
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    char str1[100];  
+    cout << "Enter your string: ";
+    cin.getline(str1, 100);  
+    cout <<str1 << endl;
+    return 0;
+}
